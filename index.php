37328d6346f7b191346ded6c664753b49dae36bd
StephaneMobiArchitects <100561681+StephaneMobiArchitects@users.noreply.github.com>
2023-05-02 17:53:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Test DevOps</title>
 </head>
 <body>
-    <center><?php echo '<p>HELLO KHADIJA AAAAAAAAAAAAA </p>'; ?>
+    <center><?php echo '<p>WELCOME TO AZURE DEVOPS : PIPELINE BE LIKE </p>'; ?>
  </center>
 
 </body>
