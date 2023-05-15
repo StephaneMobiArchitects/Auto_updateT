202a283a4ae69c7db014385d3ad8a97c74e0a54d
StephaneMobiArchitects <100561681+StephaneMobiArchitects@users.noreply.github.com>
2023-05-15 17:58:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Test DevOps</title>
 </head>
 <body>
-    <center><?php echo '<p>WELCOME TO AZURE DEVOPS : GO  </p>'; ?>
+    <center><?php echo '<p>WELCOME TO AZURE DEVOPS : GOGO  </p>'; ?>
  </center>
 
 </body>
